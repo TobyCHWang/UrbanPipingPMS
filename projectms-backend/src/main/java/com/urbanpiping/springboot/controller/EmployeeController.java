73f1ddd09524126b9132f75b9de5b7dc3744074e
kristelaquiatan <kristelandicho@gmail.com>
2022-06-07 21:26:42 -0600

    createdEmployeeTable060722

--- projectms-backend/src/main/java/com/urbanpiping/springboot/controller/EmployeeController.java
+++ projectms-backend/src/main/java/com/urbanpiping/springboot/controller/EmployeeController.java
@@ -1,5 +1,25 @@
 package com.urbanpiping.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.urbanpiping.springboot.model.Employee;
+import com.urbanpiping.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
 }
